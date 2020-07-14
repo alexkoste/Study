8a7cba4572b2d281670e7b6fc056abc365676f2f
alexkoste <34475394+alexkoste@users.noreply.github.com>
2020-07-14 10:22:25 +0300

    Minor fixes

--- AK.UIP.HW1/AK.UIP.HW1.PointsAndLines/Program.cs
+++ AK.UIP.HW1/AK.UIP.HW1.PointsAndLines/Program.cs
@@ -1,10 +1,5 @@
 ﻿using AK.UIP.HW1.PointsAndLines.Classes;
-using AK.UIP.HW1.VehiclesConsoleApp.Vehicles;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AK.UIP.HW1.PointsAndLines
 {
