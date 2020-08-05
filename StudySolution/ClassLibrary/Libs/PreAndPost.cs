344890bf7520767eb86651928a0a450d93c1c2cc
alexkoste <34475394+alexkoste@users.noreply.github.com>
2020-08-05 13:59:06 +0300

    Libs were added

---
+++ StudySolution/ClassLibrary/Libs/PreAndPost.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Libs
+{
+    public class PreAndPost
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            CustomizeTest();
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            if (webDriver!=null)    
+                webDriver.Quit();
+                webDriver.Dispose();
+        }
+
+        protected virtual void CustomizeTest() { }
+
+
+
+
+        public void InitBrowser(BrowserType browser)
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string driverFolder = Path.Combine(basePath, @"..\..\..\Drivers");
+            switch (browser)
+            {
+                case BrowserType.Chrome:
+                    _webDriver = new ChromeDriver();
+                    break;
+                case BrowserType.InternetExplorer:
+                    _webDriver = new InternetExplorerDriver(Path.Combine(driverFolder, "Ie"));
+                    break;
+                case BrowserType.FireFox:
+                    _webDriver = new FirefoxDriver();
+                    break;
+            }
+        }
+
+        public IWebDriver webDriver
+        {
+            get => _webDriver;
+        }
+
+        private IWebDriver _webDriver = null;
+    }
+
+
+
+    public enum BrowserType {
+        Chrome,
+        InternetExplorer,
+        FireFox
+    }
+}
---
+++ StudySolution/ClassLibrary/Tests/TestCase1.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary.Libs;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tests
+{
+    public class TestCase1:PreAndPost
+    {
+        protected override void CustomizeTest()
+        {
+            InitBrowser(BrowserType.InternetExplorer);
+        }
+
+        [Test]
+        public void Test1() 
+        {
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            webDriver.Url = "http://google.com";
+            
+            ;
+
+        }
+
+
+    }
+}
